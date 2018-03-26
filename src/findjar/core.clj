0d9ccf4a5d2ec38ea2e5d65210b087763e03524f
Matias Bjarland <matias@iteego.com>
2018-03-26 13:49:26 +0200

    refactoring side effects to edges

--- src/findjar/core.clj
+++ src/findjar/core.clj
@@ -10,7 +10,17 @@
            (java.util.zip ZipFile ZipEntry)
            (java.util.regex Pattern)))
 
-(defn valid-file? [^File f opts]
+(comment
+  (def output-handlers
+    {:warning     (fn [file type msg opts])
+     :match       (fn [display-name])
+     :grep-match  (fn [display-name line-nr hit? line opts])
+     :dump-stream (fn [display-name stream-factory opts])
+     :hash        (fn [display-name type hash])
+     })
+
+  )
+(defn valid-file? [^File f opts]                            ;;TODO: use multimethod file type
   (let [valid-type (if (-> f .getName (.endsWith ".jar")) \j \f)
         type       (:type opts)]
     (and
@@ -135,6 +145,13 @@
       cat (dump-stream stream-factory display-name opts)
       grep (grep-stream stream-factory display-name opts))))
 
+(defn file-ext [^File f]
+  (let [p (.getCanonicalPath f)
+        i (.lastIndexOf p ".")]
+    (if (pos? i)
+      (subs p i)
+      nil)))
+
 ; TODO: make this a multi-method to enables more file formats
 ; TODO: make -type accept a set to support "-t fjg" for files, jar files, gzip files etc
 (defn find-in-jar [^File f display-name opts]
@@ -147,18 +164,63 @@
               stream-factory #(.getInputStream zip entry)]
           (print-stream-matches opts entry-name display-name stream-factory))))
     (catch Exception e
-      (println "error opening jar file" (.getPath f) ", error:" (.getMessage e))
+      (println "error opening" (.getPath f) ", error:" (.getMessage e))
       (throw e))))
 
-(defn find-in-file [^File f ^File root opts]
+;; extensible file type handlers
+(defmulti cli-file-type identity)
+(defmethod cli-file-type "jar" [_]
+  {:type-char         \j
+   :desc              "jar files"
+   :search-by-default true})
+
+(defmethod cli-file-type "zip" [_]
+  {:type-char         \z
+   :desc              "zip files"
+   :search-by-default false})
+
+(defmethod cli-file-type :default [_]
+  {:type-char         \d
+   :desc              "disk files"
+   :search-by-default true})
+
+(defmulti find-in-file-type
+          "todo: implement
+          [if file type specified
+            then use only the specified file types
+            else use search-by-default file types
+            this will come in as a list of :active-filetypes
+            in opts"
+          (fn [^File f display-name opts] (file-ext f)))
+
+(defmethod find-in-file-type "jar" [^File f display-name opts]
+  (find-in-jar f display-name opts))
+
+(defmethod find-in-file-type "zip" [^File f display-name opts]
+  (find-in-jar f display-name opts))
+
+(defmethod find-in-file-type :default [^File f display-name opts]
+  (let [file-name      (.getName f)
+        stream-factory #(jio/input-stream f)]
+    (print-stream-matches opts file-name display-name stream-factory)))
+
+;; extensible file hashing
+(defmulti cli-hash-type identity)
+(defmethod cli-hash-type :md5 [type]
+  {:type-char \m
+   :desc      "md5 hash"})
+(defmethod cli-hash-type :sha1 [type]
+  {:type-char \s
+   :desc      "sha1 hash"})
+
+
+
+
+(defn find-in-file [^File f ^File root opts handlers]
   (when (valid-file? f opts)
     (let [display-name (if (:apath opts) (.getCanonicalPath f)
-                                         (relative-path root f))
-          file-name    (.getName f)]
-      (if (some  #(.endsWith file-name %) [".jar" ".zip"])
-        (find-in-jar f display-name opts)
-        (let [stream-factory #(jio/input-stream f)]
-          (print-stream-matches opts file-name display-name stream-factory))))))
+                                         (relative-path root f))]
+      (find-in-file-type f display-name opts))))
 
 (defn munge-regexes [opts]
   (let [{:keys [flags name grep path apath]} opts]
@@ -174,17 +236,37 @@
           opts
           [:name :grep :path :apath])))))
 
-(defn perform-file-scan [opts]
+(defn perform-file-scan [opts handlers]
   (let [root (:search-root opts)
         opts (munge-regexes opts)]
     (doseq [^File f (filter #(-> % .isFile) (file-seq root))]
-      (find-in-file f root opts))))
+      (find-in-file f root opts handlers))))
+
+(defn build-output-handlers []
+  {:warning     (fn [file type msg opts]
+                  (println "WARN: can not read file -" (.getPath file))
+
+                  :match (fn [display-name]
+                           (println display-name))
+
+                  :grep-match (fn [display-name line-nr hit? line opts]
+                                (println (str (str/trim display-name)
+                                              ":" (inc line-nr) (if hit? ">" ":")
+                                              (str/trim line)))))
+   :dump-stream dump-stream
+   :hash        (fn [display-name type hash])}
+                  ;(println (sha1 stream) display-name))}
+  )
 
 (defn -main [& args]
-  (let [{:keys [search-root opts exit-message ok?]} (cli/validate-args args)]
+  (let [{:keys [search-root opts exit-message ok?]} (cli/validate-args args)
+        output-handlers {}
+        opts (assoc opts :search-root search-root)
+
+        ]
     (if exit-message
       (cli/exit (if ok? 0 1) exit-message)
-      (perform-file-scan (assoc opts :search-root search-root)))))
+      (perform-file-scan opts output-handlers))))
 
 (defn repl-main [& args]
   (let [{:keys [search-root opts exit-message ok?]} (cli/validate-args args)]
