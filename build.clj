10bddd462f82f406cf7f03b2268632d75b458d56
Matias Bjarland <matias@iteego.com>
2022-11-03 16:25:14 +0100

    working on tools.build build

---
+++ build.clj
@@ -0,0 +1,33 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+
+(def lib 'mbjarland/findjar)
+(def version "1.0.4")                                       ;(format "1.0.4.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn generate-version-file [_]
+  (let [hash  (b/git-process {:git-args ["rev-parse" "HEAD"]})
+        short (b/git-process {:git-args ["rev-parse" "--short" "HEAD"]})
+        ccount (b/git-count-revs nil)])
+  )
+
+(b/git-count-revs nil)
+
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs   ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis     basis
+                  :src-dirs  ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis     basis
+           :main      'findjar.main}))
